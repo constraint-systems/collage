87cac29169877ef13e267ad8c50b4d984a821c10
Grant Custer <grantcuster@gmail.com>
2021-11-20 09:32:38 -0500

    set file accept

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -264,7 +264,7 @@ export default function UploadTest({
           </div>
         </div>
       </div>
-      <FileInput onChange={handleFileChange} />
+      <FileInput onChange={handleFileChange} accept=".jpg,.png,.jpeg" />
       <div
         role="button"
         title="Add an image"
