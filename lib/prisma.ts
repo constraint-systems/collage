3857b81319ba4186d65e86a4aab8387593bb9e23
Grant Custer <grantcuster@gmail.com>
2021-11-19 21:02:44 -0500

    fix build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,12 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  var prisma: PrismaClient;
 }
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export default prisma;
